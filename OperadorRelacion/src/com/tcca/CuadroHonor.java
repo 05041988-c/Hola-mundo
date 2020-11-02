3a6ac0c7f299850ca94c9245334d95c2d8e6c41b
IBETH <72093876+05041988-c@users.noreply.github.com>
2020-11-02 14:45:09 -0600

    Add files via upload
    Ejemplo de operadores de relación   >= valor
    • Los valores se prueban de cada lado del operador y se devuelve un valor verdadero o falso.
    Los valores booleanos son necesarios como una condición en una sentencia if-else o bucle.

---
+++ OperadorRelacion/src/com/tcca/CuadroHonor.java
@@ -0,0 +1,14 @@
+package com.tcca;
+
+public class CuadroHonor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int grade = 90;
+		boolean madeHonorRoll = grade >= 88;
+		if(madeHonorRoll)
+		System.out.println("Estas en el cuadro de honbor.");
+
+	}
+
+}
