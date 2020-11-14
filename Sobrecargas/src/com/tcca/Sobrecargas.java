aabd375e8e90568b5cddfda3ecca99cbf8959880
IBETH <72093876+05041988-c@users.noreply.github.com>
2020-11-13 18:22:58 -0600

    Add files via upload
    Ejemplo de sobrecarga

---
+++ Sobrecargas/src/com/tcca/Sobrecargas.java
@@ -0,0 +1,13 @@
+package com.tcca;
+
+public class Sobrecargas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Automovil ferrari = new Automovil(10000, "ferrari", 4);
+	}
+				
+			}
+
+
